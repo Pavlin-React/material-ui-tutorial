38729dcf834026d308d15e890e5fd021eed1d66e
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-27 10:40:13 +0300

    Simple App Bar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import { Button, Typography, makeStyles } from '@material-ui/core'
-import './App.css';
+
+import NavBar from './components/NavBar'
+
 
 function App() {
-
-  let useStyle = makeStyles( {
-    helloStyle: {
-      color: 'gray'
-    }
-  } )
-  let classes = useStyle()
-  return (
-    
-    <div className="App">
-      <Typography className={ classes.helloStyle }
-      color='initial'
-      variant='h4'
-      >
-        Hello There
-      </Typography>
-      <Button
-      onClick={ () => { alert( 'You just clicked me !!!' ) } }
-      variant='contained'
-      color='primary'
-      >This is my first button</Button>
-    </div>
-  );
+  return(
+    <NavBar />
+  )
 }
 
 export default App;
