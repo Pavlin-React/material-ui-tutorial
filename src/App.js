2a3f13160daeb3bbf3501580a88997f8c6a6d7d5
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-27 09:37:01 +0300

    Typography, makeStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { Button } from '@material-ui/core'
+import { Button, Typography, makeStyles } from '@material-ui/core'
 import './App.css';
 
 function App() {
+
+  let useStyle = makeStyles( {
+    helloStyle: {
+      color: 'gray'
+    }
+  } )
+  let classes = useStyle()
   return (
+    
     <div className="App">
+      <Typography className={ classes.helloStyle }
+      color='initial'
+      variant='h4'
+      >
+        Hello There
+      </Typography>
       <Button
+      onClick={ () => { alert( 'You just clicked me !!!' ) } }
       variant='contained'
       color='primary'
       >This is my first button</Button>
