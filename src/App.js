b1b9b5f9a9f1fbf4969a0c664a3c228d11c406af
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-28 10:41:56 +0300

    Grid + Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
+import { Grid } from '@material-ui/core'
 import NavBar from './components/NavBar'
 
 
 
 function App() {
   return(
-    <div>
-      <NavBar />
+    <Grid container spacing={ 10 }>
+      <Grid  item xs={ 12 } sm={ 6 } md={ 3 } lg={ 3 }><NavBar /></Grid>
+      <Grid  item xs={ 12 } sm={ 6 } md={ 3 } lg={ 3 }><NavBar /></Grid>
+      <Grid  item xs={ 12 } sm={ 6 } md={ 3 } lg={ 3 }><NavBar /></Grid>
       
-    </div>
+    </Grid>
   )
 }
 
